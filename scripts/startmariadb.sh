7340196b729f64e66459d8932e26cee357e7aacc
Mark Stillwell <mark@stillwell.me>
2014-10-30 22:58:53 +0000

    another typo...

--- scripts/startmariadb.sh
+++ scripts/startmariadb.sh
@@ -3,7 +3,8 @@
 : ${mysql_root_password:="password"}
 : ${docker_host:="localhost"}
 
-service start mariadb
+service mysql start
+
 ansible-playbook -i inventories/local.ini playbooks/configure.yml -e "{
     'mariadb_mysql_root_password' : '${mysql_root_password}',
     'mariadb_docker_host' : '${docker_host}' }"
