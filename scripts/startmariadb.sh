126c51e80dec4f6f5bb991bf39ed78ba851d7123
Mark Stillwell <mark@stillwell.me>
2014-10-31 00:11:05 +0000

    okay, let's try this...

--- scripts/startmariadb.sh
+++ scripts/startmariadb.sh
@@ -3,6 +3,9 @@
 : ${mysql_root_password:="password"}
 : ${docker_host:="localhost"}
 
+echo $mysql_root_password
+echo $docker_host
+
 service mysql start
 
 ansible-playbook -i inventories/local.ini provisioning/configure.yml -e "{
