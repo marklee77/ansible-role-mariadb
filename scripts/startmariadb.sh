147e5195f8bc56075a860e920046949fbd346301
Mark Stillwell <mark@stillwell.me>
2014-10-31 00:35:31 +0000

    do we need mysql-client?

--- scripts/startmariadb.sh
+++ scripts/startmariadb.sh
@@ -3,9 +3,6 @@
 : ${mysql_root_password:="password"}
 : ${docker_host:="localhost"}
 
-echo $mysql_root_password
-echo $docker_host
-
 service mysql start
 
 ansible-playbook -i inventories/local.ini provisioning/configure.yml -e "{
